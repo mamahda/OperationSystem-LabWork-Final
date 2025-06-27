6d2f7af7de6de839b5065078f1e733675ea81a9c
Ctrl-Zack <zakyzein1922@gmail.com>
2025-06-27 21:18:23 +0700

    sementara

--- src/shell.c
+++ src/shell.c
@@ -3,6 +3,8 @@
 #include "std_lib.h"
 #include "filesystem.h"
 
+// int parsePath(char* path, char dirs[][MAX_FILENAME]);
+
 void shell() {
   char buf[64];
   char cmd[64];
@@ -48,7 +50,7 @@ void printCWD(byte cwd) {
   }
 
   printString("/");
-  for (i = depth - 1; i >= 0; i--) {
+  for (i = depth - 1; i >= 0; --i) {
     printString(path[i]);
     if (i > 0) printString("/");
   }
@@ -62,13 +64,15 @@ void parseCommand(char* buf, char* cmd, char arg[2][64]) {
   memset(arg[0], 0, 64);
   memset(arg[1], 0, 64);
   
-  while (buf[i] == ' ') i++;
+  while (buf[i] == ' ') i++; //* ignore space
 
   while (buf[i] != ' ' && buf[i] != '\0') cmd[j++] = buf[i++];
   cmd[j] = '\0';
 
+  if (buf[i] == '\0') return;
+
   for (k = 0; k < 2; k++) {
-    while (buf[i] == ' ') i++;
+    while (buf[i] == ' ') i++; //* ignore space
 
     j = 0;
     while (buf[i] != ' ' && buf[i] != '\0') arg[k][j++] = buf[i++];
@@ -79,23 +83,237 @@ void parseCommand(char* buf, char* cmd, char arg[2][64]) {
 
 // TODO: 6. Implement cd function
 void cd(byte* cwd, char* dirname) {
+  struct node_fs node_fs_buf;
+  int i;
 
+  readSector(&(node_fs_buf.nodes[0]), FS_NODE_SECTOR_NUMBER);
+  readSector(&(node_fs_buf.nodes[32]), FS_NODE_SECTOR_NUMBER + 1);
+
+  if (strcmp(dirname, "/")) {
+    *cwd = FS_NODE_P_ROOT;
+    return;
+  }
+
+  if (strcmp(dirname, "..")) {
+    if (*cwd != FS_NODE_P_ROOT) *cwd = node_fs_buf.nodes[*cwd].parent_index;
+    return;
+  }
+
+  for (i = 0; i < FS_MAX_NODE; ++i) {
+    if (strcmp(node_fs_buf.nodes[i].node_name, dirname) && node_fs_buf.nodes[i].parent_index == *cwd) {
+      if (node_fs_buf.nodes[i].data_index == FS_NODE_D_DIR) *cwd = i;
+      else {
+        printString("cd: "); printString("not a directory: "); printString(dirname); printString("\n");
+      }
+      return;
+    }
+  }
+
+  printString("cd: "); printString("no such file or directory: "); printString(dirname); printString("\n");
 }
 
 // TODO: 7. Implement ls function
 void ls(byte cwd, char* dirname) {
+  struct node_fs node_fs_buf;
+  int i, j, dir_count = 0;
+  bool found = false;
+  byte dir_idx = cwd;
+  char dirs[4][MAX_FILENAME];
 
+  
+  readSector(&(node_fs_buf.nodes[0]), FS_NODE_SECTOR_NUMBER);
+  readSector(&(node_fs_buf.nodes[32]), FS_NODE_SECTOR_NUMBER + 1);
+  
+  if (strlen(dirname) == 0 || strcmp(dirname, ".")) found = true;
+  else {
+    dir_count = parsePath(dirname, dirs);
+
+    if (strlen(dirname) == 0 || strcmp(dirname, ".")) {
+      // do nothing
+    } else {
+      dir_count = parsePath(dirname, dirs);
+      for (i = 0; i < dir_count; i++) {
+        found = false;
+        for (j = 0; j < FS_MAX_NODE; j++) {
+          if (strcmp(node_fs_buf.nodes[j].node_name, dirs[i]) && node_fs_buf.nodes[j].parent_index == dir_idx && node_fs_buf.nodes[j].data_index == FS_NODE_D_DIR) {
+            dir_idx = j;
+            found = true;
+            break;
+          }
+        }
+        if (!found) break;
+      }
+    }
+  }
+
+  if (!found) {
+    printString("ls: cannot access '"); printString(dirname); printString("': no such file or directory\n");
+    return;
+  }
+
+  for (i = 0; i < FS_MAX_NODE; ++i) {
+    if (node_fs_buf.nodes[i].parent_index == dir_idx && strlen(node_fs_buf.nodes[i].node_name) > 0) {
+      printString(node_fs_buf.nodes[i].node_name); printString(" ");
+    }
+  }
+  
+  printString("\n");
 }
 
 // TODO: 8. Implement mv function
 void mv(byte cwd, char* src, char* dst) {
+  struct node_fs node_fs_buf;
+  int i;
+  byte src_idx = FS_NODE_P_ROOT;
+  byte target_dir;
+  char output_name[MAX_FILENAME];
 
+  readSector(&(node_fs_buf.nodes[0]), FS_NODE_SECTOR_NUMBER);
+  readSector(&(node_fs_buf.nodes[32]), FS_NODE_SECTOR_NUMBER + 1);
+
+  for (i = 0; i < FS_MAX_NODE; i++) {
+    if (strcmp(node_fs_buf.nodes[i].node_name, src) && node_fs_buf.nodes[i].parent_index == cwd && node_fs_buf.nodes[i].data_index != FS_NODE_D_DIR) {
+      src_idx = i;
+      break;
+    }
+  }
+
+  if (src_idx == FS_NODE_P_ROOT) {
+    printString("mv: cannot stat '"); printString(src); printString("': No such file\n");
+    return;
+  }
+
+  if (!resolvePathAndName(&node_fs_buf, cwd, dst, &target_dir, output_name)) {
+    printString("mv: destination path not found\n");
+    return;
+  }
+
+  for (i = 0; i < FS_MAX_NODE; i++) {
+    if (strcmp(node_fs_buf.nodes[i].node_name, output_name) && node_fs_buf.nodes[i].parent_index == target_dir) {
+      printString("mv: file with same name already exists\n");
+      return;
+    }
+  }
+
+  if (strcmp(node_fs_buf.nodes[src_idx].node_name, output_name) && node_fs_buf.nodes[src_idx].parent_index == target_dir) {
+    printString("mv: '"); printString(src);
+    printString("' and '"); printString(dst);
+    printString("' are the same file\n");
+    return;
+  }
+
+  node_fs_buf.nodes[src_idx].parent_index = target_dir;
+  strcpy(node_fs_buf.nodes[src_idx].node_name, output_name);
+
+  writeSector(&(node_fs_buf.nodes[0]), FS_NODE_SECTOR_NUMBER);
+  writeSector(&(node_fs_buf.nodes[32]), FS_NODE_SECTOR_NUMBER + 1);
+
+  printString("mv: file moved successfully\n");
 }
 
 // TODO: 9. Implement cp function
+// TODO: 9. Implement cp function
 void cp(byte cwd, char* src, char* dst) {
+    struct node_fs node_fs_buf;
+    struct file_metadata src_metadata;
+    struct file_metadata dst_metadata;
+    enum fs_return status;
 
+    int i;
+    byte target_dir = cwd;
+    char output_name[MAX_FILENAME];
+    char dir_name[MAX_FILENAME];
+    int outnamestart = 0;
+    int slashidx;
+
+    readSector(&(node_fs_buf.nodes[0]), FS_NODE_SECTOR_NUMBER);
+    readSector(&(node_fs_buf.nodes[32]), FS_NODE_SECTOR_NUMBER + 1);
+
+    for (i = 0; i < FS_MAX_NODE; i++) {
+        if (node_fs_buf.nodes[i].parent_index == cwd &&
+            strcmp(node_fs_buf.nodes[i].node_name, src) == 0 &&
+            node_fs_buf.nodes[i].data_index != FS_NODE_D_DIR) {
+            src_metadata.parent_index = cwd;
+            strcpy(src_metadata.node_name, src);
+            fsRead(&src_metadata, &status);
+            if (status != FS_SUCCESS) {
+                printString("Error reading source file\n");
+                return;
+            }
+            break;
+        }
+    }
+
+    if (i == FS_MAX_NODE) {
+        printString("Source file not found\n");
+        return;
+    }
+
+    if (dst[0] == '/') {
+        target_dir = FS_NODE_P_ROOT;
+        outnamestart = 1;
+    } else if (dst[0] == '.' && dst[1] == '.') {
+        target_dir = node_fs_buf.nodes[cwd].parent_index;
+        outnamestart = 3;
+    } else {
+        slashidx = -1;
+        for (i = 0; dst[i] != '\0'; i++) {
+            if (dst[i] == '/') {
+                slashidx = i;
+            }
+        }
+
+        if (slashidx != -1) {
+            for (i = 0; i < slashidx; i++) {
+                dir_name[i] = dst[i];
+            }
+            dir_name[slashidx] = '\0';
+
+            for (i = 0; i < FS_MAX_NODE; i++) {
+                if (node_fs_buf.nodes[i].parent_index == cwd &&
+                    strcmp(node_fs_buf.nodes[i].node_name, dir_name) == 1) {
+                    target_dir = i;
+                    break;
+                }
+            }
+
+            if (i == FS_MAX_NODE) {
+                printString("Target directory not found\n");
+                return;
+            }
+
+            outnamestart = slashidx + 1;
+        } else {
+            outnamestart = 0;
+        }
+    }
+
+    for (i = 0; dst[outnamestart + i] != '\0' && i < MAX_FILENAME - 1; i++) {
+        output_name[i] = dst[outnamestart + i];
+    }
+    output_name[i] = '\0';
+
+    for (i = 0; i < FS_MAX_NODE; i++) {
+        if (node_fs_buf.nodes[i].parent_index == target_dir &&
+            strcmp(node_fs_buf.nodes[i].node_name, output_name) == 1) {
+            printString("File with the same name already exists\n");
+            return;
+        }
+    }
+
+    dst_metadata.parent_index = target_dir;
+    strcpy(dst_metadata.node_name, output_name);
+    dst_metadata.filesize = src_metadata.filesize;
+    memcpy(dst_metadata.buffer, src_metadata.buffer, src_metadata.filesize);
+
+    fsWrite(&dst_metadata, &status);
+    if (status != FS_SUCCESS) {
+        printString("Error writing copied file\n");
+    } else {
+        printString("File copied successfully\n");
+    }
 }
+
 
 // TODO: 10. Implement cat function
 void cat(byte cwd, char* filename) {
@@ -129,6 +347,37 @@ void cat(byte cwd, char* filename) {
 
 // TODO: 11. Implement mkdir function
 void mkdir(byte cwd, char* dirname) {
+  struct node_fs node_fs_buf;
+  int i, empty_node_idx = -1;
 
+  readSector(&(node_fs_buf.nodes[0]), FS_NODE_SECTOR_NUMBER);
+  readSector(&(node_fs_buf.nodes[32]), FS_NODE_SECTOR_NUMBER + 1);
+
+  for (i = 0; i < FS_MAX_NODE; ++i) {
+    if (strcmp(node_fs_buf.nodes[i].node_name, dirname) && node_fs_buf.nodes[i].parent_index == cwd) {
+      printString("mkdir: directory or file already exists\n");
+      return;
+    }
+  }
+
+  for (i = 0; i < FS_MAX_NODE; ++i) {
+    if (node_fs_buf.nodes[i].node_name[0] == '\0') {
+      empty_node_idx = i;
+      break;
+    }
+  }
+
+  if (empty_node_idx == -1) {
+    printString("mkdir: no space for new directory\n");
+    return;
+  }
+
+  node_fs_buf.nodes[empty_node_idx].parent_index = cwd;
+  node_fs_buf.nodes[empty_node_idx].data_index = FS_NODE_D_DIR;
+  strncpy(node_fs_buf.nodes[empty_node_idx].node_name, dirname, MAX_FILENAME);
+
+  writeSector(&(node_fs_buf.nodes[0]), FS_NODE_SECTOR_NUMBER);
+  writeSector(&(node_fs_buf.nodes[32]), FS_NODE_SECTOR_NUMBER + 1);
+
+  printString("mkdir: directory created successfully\n");
 }
-
